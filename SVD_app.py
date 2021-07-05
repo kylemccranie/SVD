799f1054ee5f72916f6369e0138709556d76c3f3
kylemccranie <46943571+kylemccranie@users.noreply.github.com>
2021-07-05 16:38:28 -0500

    Add files via upload

---
+++ SVD_app.py
@@ -0,0 +1,155 @@
+import openpyxl
+import csv
+from openpyxl import load_workbook
+import xlsxwriter
+
+Title = []
+IssueType = []
+ComponentVariant = []
+DRNumber = []
+
+numberOfRows = 0
+
+
+#caluculate max number of rows in excel sheet for later functions
+def calcNumberOfRows():
+    global numberOfRows
+    wb = load_workbook(filename = 'file.xlsx')
+    ws = wb.active
+    for row in range(2,ws.max_row + 1):
+           numberOfRows = numberOfRows + 1
+    return numberOfRows
+    
+
+
+#code converting csv to xlsx so that we can use openpyxl
+def convert():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    with open('JiraExport.csv') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            ws.append(row)
+
+    wb.save('file.xlsx')
+
+
+#setting up the template sheet
+def create_Template():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws['A1'] = "Type"
+    ws['B1'] = "DR Number"
+    ws['C1'] = "Internal ID"
+    ws['D1'] = "Component"
+    ws['E1'] = "CVV Build #"
+    ws['F1'] = "Title"
+    ws['G1'] = "Description"
+    ws['H1'] = "Problem Path"
+    ws['I1'] = "Acceptance Criteria"
+    ws['J1'] = "UI Affected (If yes then how?)"
+    ws['K1'] = "Change Summary"
+    ws['L1'] = "Fix Comments"
+    wb.save('template_out.xlsx')
+    
+            
+#read and store 'Summary/title' into Title Array
+def readToTitle():
+    wb = load_workbook(filename = 'file.xlsx')
+    ws = wb.active
+    for col in ws.iter_cols(min_row=2, max_col=1, max_row=numberOfRows+1, values_only = True):
+        for cell in col:
+            Title.append(cell)
+
+##Writing Summary/Title Array to template sheet
+def writeTitleToTemplate():
+    x = 2 
+    wb = load_workbook(filename = 'template_out.xlsx')
+    ws = wb.active
+    for i in Title:
+        ws.cell(row = x, column = 6).value = i
+        x = x + 1
+    wb.save('template_out.xlsx')
+
+
+        
+#read and store 'issue type' into  Array
+def readToIsssueType():
+    wb = load_workbook(filename = 'file.xlsx')
+    ws = wb.active
+    for col in ws.iter_cols(min_row=2,min_col = 4, max_col=4, max_row=numberOfRows+1, values_only = True):
+        for cell in col:
+            IssueType.append(cell)
+
+##Writing IssueType Array to template sheet
+def writeIssueTypeToTemplate():
+    x = 2 
+    wb = load_workbook(filename = 'template_out.xlsx')
+    ws = wb.active
+    for i in IssueType:
+        ws.cell(row = x, column = 1).value = i
+        x = x + 1
+    wb.save('template_out.xlsx')
+
+##Writing Component Variant from export to Component Variant Array
+def readToComponentVariant():
+    wb = load_workbook(filename = 'file.xlsx')
+    ws = wb.active
+    for col in ws.iter_cols(min_row=2,min_col = 65, max_col=65, max_row=numberOfRows+1, values_only = True):
+        for cell in col:
+            ComponentVariant.append(cell)
+
+##Writing Component Variant Array to template
+def writeComponentVariantToTemplate():
+    x = 2 
+    wb = load_workbook(filename = 'template_out.xlsx')
+    ws = wb.active
+    for i in ComponentVariant:
+        ws.cell(row = x, column = 4).value = i
+        x = x + 1
+    wb.save('template_out.xlsx')
+
+
+##Writing DRNumber from export to DRNumber Array
+def readToDRNumber():
+    wb = load_workbook(filename = 'file.xlsx')
+    ws = wb.active
+    for col in ws.iter_cols(min_row=2,min_col = 71, max_col=71, max_row=numberOfRows+1, values_only = True):
+        for cell in col:
+            DRNumber.append(cell)
+
+##Writing Component Variant Array to template
+def writeDRNumberToTemplate():
+    x = 2 
+    wb = load_workbook(filename = 'template_out.xlsx')
+    ws = wb.active
+    for i in DRNumber:
+        ws.cell(row = x, column = 2).value = i
+        x = x + 1
+    wb.save('template_out.xlsx')
+
+
+##Test Function for inserting values
+##def test():
+    ##wb = load_workbook(filename = 'template_out.xlsx')
+    ##ws = wb.active
+    ##ws.cell(row=2, column=2).value = 2
+    ##wb.save('template_out.xlsx')
+
+        
+
+calcNumberOfRows()
+convert()
+create_Template()
+readToTitle()
+writeTitleToTemplate()
+readToIsssueType()
+writeIssueTypeToTemplate()
+readToComponentVariant()
+writeComponentVariantToTemplate()
+readToDRNumber()
+writeDRNumberToTemplate()
+
+
+
